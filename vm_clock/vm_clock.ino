a2a72830af55682820fbb42bd4f5cb4803d7db42
Lily Romano <Lilyheart@users.noreply.github.com>
2021-08-08 13:04:35 -0400

    Initial rewrite working

--- vm_clock/vm_clock.ino
+++ vm_clock/vm_clock.ino
@@ -1,4 +1,5 @@
-#include "RTClib.h" // https://github.com/adafruit/RTClib
+#include <RTClib.h> // https://github.com/adafruit/RTClib
+#include <Wire.h>
 
 const int secPin = 6;
 const int minPin = 5;
@@ -9,13 +10,11 @@ const int PMLEDPin = 11;
 bool isSecDecay = false;
 bool isMinDecay = false;
 bool isHourDecay = false;
-bool isAMLEDDecay = false;
-bool isPMLEDDecay = false;
-int decayPins[] = {secPin, minPin, hourPin, AMLEDPin, PMLEDPin}; // order must match isDecayPins in Decay method
+int decayPins[] = {secPin, minPin, hourPin}; // order must match isDecayPins in Decay method
 
-int secReset = 0;
-int minReset = 0;
-int hourReset = 0;
+int secReset = 1;
+int minReset = 1;
+int hourReset = 1;
 
 const int buttonPin1 = 8;     // the number of the pushbutton pin
 const int buttonPin2 = 7;     // the number of the pushbutton pin
@@ -40,8 +39,8 @@ RTC_DS1307 rtc;
  */
 void Decay(int startVal, int endVal, int decayRate, int delayTime) {
   int decay = startVal;
-  bool isDecayPins[] = {isSecDecay, isMinDecay, isHourDecay, isAMLEDDecay, isPMLEDDecay};
-  bool isResets[] = {secReset, minReset, hourReset, 0, 0};
+  bool isDecayPins[] = {isSecDecay, isMinDecay, isHourDecay};
+  bool isResets[] = {secReset, minReset, hourReset};
 
   // Loop through the decays
   for (int i = 0; i <= abs(startVal - endVal); i++) {
@@ -56,6 +55,10 @@ void Decay(int startVal, int endVal, int decayRate, int delayTime) {
 
     delay(delayTime);
     decay += decayRate;
+
+    if (decay < 0) {
+      decay = 0;
+    }
   }
 }
 
@@ -68,19 +71,31 @@ void InitializeButton() {
 
 void InitializePin() {
   // ## INITIALIZE PINS
+  pinMode(AMLEDPin, OUTPUT);
+  pinMode(PMLEDPin, OUTPUT);
+
   isSecDecay = true;
   isMinDecay = true;
   isHourDecay = true;
-  isAMLEDDecay = true;
-  isPMLEDDecay = true;
 
+  digitalWrite(AMLEDPin, HIGH);
+  digitalWrite(PMLEDPin, LOW);
   Decay(0, 255,  1, 10);
+
+  digitalWrite(AMLEDPin, LOW);
+  digitalWrite(PMLEDPin, HIGH);
   Decay(255, 0, -1, 10);
+
+  secReset = 0;
+  minReset = 0;
+  hourReset = 0;
 }
 
 void setup() {
 
   Serial.begin(9600);
+  Wire.begin();
+  rtc.begin();
 
   InitializeButton();
   InitializePin();
@@ -96,9 +111,9 @@ void setup() {
  */
 
 void buttonLoop() {
-  buttonState1 = digitalRead(buttonPin1);
-  buttonState2 = digitalRead(buttonPin2);
-
+//  buttonState1 = digitalRead(buttonPin1);
+//  buttonState2 = digitalRead(buttonPin2);
+//
 //  if ((buttonState1 == HIGH) && (hp1 == 0)) {
 //    Serial.println("Pressed1");
 //    setTime(hour() + 1, minute(), second(), day(), month(), year());
@@ -124,14 +139,12 @@ void displayTime() {
   isSecDecay = false;
   isMinDecay = false;
   isHourDecay = false;
-  isAMLEDDecay = false;
-  isPMLEDDecay = false;
 
   // Retrieve the time from the real time clock.
   DateTime now = rtc.now();
 
   // ### For debuging purposes, write the time to the
-  // Serial.print(h);
+  // Serial.print(now.hour());
   // Serial.print(":");
   // Serial.print(now.minute());
   // Serial.print(":");
@@ -157,25 +170,29 @@ void displayTime() {
     analogWrite(minPin, now.minute() * (255 / 60));
   }
 
-  if (now.hour() % 12 == 0) {
+  if ((now.hour() + 1) % 12 == 0) {
     if (hourReset == 0) {
       isHourDecay = true;
       hourReset = 1;
     }
   } else {
     hourReset = 0;
-    analogWrite(hourPin, (now.hour() % 12) * (255 / 12));
+    if (now.hour() <= 12) {
+      analogWrite(hourPin, now.hour() * (255 / 12));
+    } else {
+      analogWrite(hourPin, (now.hour() - 12) * (255 / 12));
+    }
   }
 
   if (now.hour() < 12) {
-    analogWrite(AMLEDPin, 250);
-    analogWrite(PMLEDPin, 0);
+    digitalWrite(AMLEDPin, HIGH);
+    digitalWrite(PMLEDPin, LOW);
   } else {
-    analogWrite(AMLEDPin, 0);
-    analogWrite(PMLEDPin, 250);
+    digitalWrite(AMLEDPin, LOW);
+    digitalWrite(PMLEDPin, HIGH);
   }
 
-  Decay(255, 0, 2, 3);
+  Decay(255, 0, -2, 4);
 }
 
 void loop() {
