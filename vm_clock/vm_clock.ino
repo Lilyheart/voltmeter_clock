e9d5a9219c22cb68641d91112e9bc24ca6e7993d
Lily Romano <Lilyheart@users.noreply.github.com>
2021-08-08 12:12:24 -0400

    Initial rewrite (doesn't work yet)

--- vm_clock/vm_clock.ino
+++ vm_clock/vm_clock.ino
@@ -1,101 +1,185 @@
-// Import necessary libs
+#include "RTClib.h" // https://github.com/adafruit/RTClib
 
-#include <RTClib.h>
-#include <Wire.h>
+const int secPin = 6;
+const int minPin = 5;
+const int hourPin = 3;
+const int AMLEDPin = 10;
+const int PMLEDPin = 11;
 
-// Factors used to derive the correct time
-// sec and mins are 60 increments of 0.083v
-// analog values are 255 divided by 60 = 4.25
-// hours are 12 increments of 21.25
-// analog hour values are 255 divided by 12 = 21.25
+bool isSecDecay = false;
+bool isMinDecay = false;
+bool isHourDecay = false;
+bool isAMLEDDecay = false;
+bool isPMLEDDecay = false;
+int decayPins[] = {secPin, minPin, hourPin, AMLEDPin, PMLEDPin}; // order must match isDecayPins in Decay method
+
+int secReset = 0;
+int minReset = 0;
+int hourReset = 0;
+
+const int buttonPin1 = 8;     // the number of the pushbutton pin
+const int buttonPin2 = 7;     // the number of the pushbutton pin
+
+int buttonState1 = 0;         // variable for reading the pushbutton status
+int buttonState2 = 0;         // variable for reading the pushbutton status
+
+int hp1 = 0;
+int hp2 = 0;
 
 RTC_DS1307 rtc;
-float sec_factor = (4.25);
-float min_factor = (4.25);
-float hrs_factor = (21.25); // This is 255 (i.e. 5v) divided by 12hrs gives 12hr time
 
-float h = 0;
-// define pinouts for relay and two push buttons
-// button 1 is hours
-// button 2 is mins
+/**
+  * Changes the meter over a range of values. All pins will change to the same
+     value at relatively the same time
+  *
+  * @param int startVal   The starting value
+  * @param int endVal     The ending value
+  * @param int decayRate  The rate of change on each `delayTime`
+                          (positive goes up, negative goes down)
+  * @param int delayTime  The delay between changes
+ */
+void Decay(int startVal, int endVal, int decayRate, int delayTime) {
+  int decay = startVal;
+  bool isDecayPins[] = {isSecDecay, isMinDecay, isHourDecay, isAMLEDDecay, isPMLEDDecay};
+  bool isResets[] = {secReset, minReset, hourReset, 0, 0};
 
-const int buttonPin1 = 2;
-const int buttonPin2 = 4;
+  // Loop through the decays
+  for (int i = 0; i <= abs(startVal - endVal); i++) {
+    // Loop through the pins
+    for (int j = 0; j < sizeof decayPins/sizeof decayPins[0]; j++) {
+      if (isDecayPins[j]) {
+        if (isResets[j] == 1) {
+          analogWrite(decayPins[j], decay);
+        }
+      }
+    }
 
-// Following are null/LOW values so that the user can adjust the hrs and mins
+    delay(delayTime);
+    decay += decayRate;
+  }
+}
 
-byte buttonState1;
-byte buttonState2;
-byte lastState1 = LOW;
-byte lastState2 = LOW;
-byte minsCount = 0;
-byte hrsCount = 0;
+void InitializeButton() {
+  // ## INITIALIZE BUTTON CODE
+
+  pinMode(buttonPin1, INPUT);
+  pinMode(buttonPin2, INPUT);
+}
+
+void InitializePin() {
+  // ## INITIALIZE PINS
+  isSecDecay = true;
+  isMinDecay = true;
+  isHourDecay = true;
+  isAMLEDDecay = true;
+  isPMLEDDecay = true;
+
+  Decay(0, 255,  1, 10);
+  Decay(255, 0, -1, 10);
+}
 
 void setup() {
 
   Serial.begin(9600);
-  Wire.begin();
-  rtc.begin();
 
+  InitializeButton();
+  InitializePin();
+}
 
-  //define inputs and outputs
-  pinMode(buttonPin1, INPUT);
-  pinMode(buttonPin2, INPUT);
+/***
+ *    ______                           _________     _________
+ *    ___  / ____________________      __  ____/___________  /____
+ *    __  /  _  __ \  __ \__  __ \     _  /    _  __ \  __  /_  _ \
+ *    _  /___/ /_/ / /_/ /_  /_/ /     / /___  / /_/ / /_/ / /  __/
+ *    /_____/\____/\____/_  .___/      \____/  \____/\__,_/  \___/
+ *                       /_/
+ */
 
+void buttonLoop() {
+  buttonState1 = digitalRead(buttonPin1);
+  buttonState2 = digitalRead(buttonPin2);
+
+//  if ((buttonState1 == HIGH) && (hp1 == 0)) {
+//    Serial.println("Pressed1");
+//    setTime(hour() + 1, minute(), second(), day(), month(), year());
+//    Serial.println(hour());
+//    hp1 = 1;
+//    delay(200);
+//  } else if (buttonState1 == LOW) {
+//    hp1 = 0;
+//  }
+//
+//  if ((buttonState2 == HIGH) && (hp2 == 0)) {
+//    Serial.println("Pressed2");
+//    setTime(hour(), minute() + 1, second(), day(), month(), year());
+//    Serial.println(minute());
+//    hp2 = 1;
+//    delay(100);
+//  } else if (buttonState2 == LOW) {
+//    hp2 = 0;
+//  }
+}
+
+void displayTime() {
+  isSecDecay = false;
+  isMinDecay = false;
+  isHourDecay = false;
+  isAMLEDDecay = false;
+  isPMLEDDecay = false;
+
+  // Retrieve the time from the real time clock.
+  DateTime now = rtc.now();
+
+  // ### For debuging purposes, write the time to the
+  // Serial.print(h);
+  // Serial.print(":");
+  // Serial.print(now.minute());
+  // Serial.print(":");
+  // Serial.println(now.second());
+
+  if (now.second() == 0) {
+    if (secReset == 0) {
+      isSecDecay = true;
+      secReset = 1;
+    }
+  } else {
+    secReset = 0;
+    analogWrite(secPin, now.second() * (255 / 60));
+  }
+
+  if (now.minute() == 0) {
+    if (minReset == 0) {
+      isMinDecay = true;
+      minReset = 1;
+    }
+  } else {
+    minReset = 0;
+    analogWrite(minPin, now.minute() * (255 / 60));
+  }
+
+  if (now.hour() % 12 == 0) {
+    if (hourReset == 0) {
+      isHourDecay = true;
+      hourReset = 1;
+    }
+  } else {
+    hourReset = 0;
+    analogWrite(hourPin, (now.hour() % 12) * (255 / 12));
+  }
+
+  if (now.hour() < 12) {
+    analogWrite(AMLEDPin, 250);
+    analogWrite(PMLEDPin, 0);
+  } else {
+    analogWrite(AMLEDPin, 0);
+    analogWrite(PMLEDPin, 250);
+  }
+
+  Decay(255, 0, 2, 3);
 }
 
 void loop() {
-
-  // float time values to be manipulated in a calculation later
-  DateTime now = rtc.now();
-  float seconds = (now.second());
-  float minutes = (now.minute());
-  float hours = (now.hour());
-  float hours2 = (now.hour()-12);
-
-  // Code for push button 1 (hours)
-
-  buttonState1 = digitalRead(buttonPin1);
-
-  if(buttonState1 && buttonState1 != lastState1)  // button latch, no debounce needed.
-  {
-    if(hrsCount < 12)
-      hrsCount += 1;
-    else
-      hrsCount = 0;
-    Serial.println(hrsCount);
-  }
-
-  lastState1 = buttonState1;
-
-// Code for push button 2 (mins)
-
-  buttonState2 = digitalRead(buttonPin2);
-
-  if(buttonState2 && buttonState2 != lastState2)  // button latch, no debounce needed.
-  {
-    if(minsCount < 60)
-      minsCount += 1;
-    else
-      minsCount = 0;
-    Serial.println(minsCount);
-  }
-
-  lastState2 = buttonState2;
-
-  // Finally write analog values to PWM pins 6, 10, and 11
-
-
-    if (hours > 12)
-    {
-      analogWrite(6, sec_factor * seconds); // Write Vout seconds
-      analogWrite(5, min_factor * (minutes + minsCount)); // Write Vout minutes
-      analogWrite(3, hrs_factor * (hours2 + hrsCount));
-    }
-    else
-    {
-      analogWrite(6, sec_factor * seconds); // Write Vout seconds
-      analogWrite(5, min_factor * (minutes + minsCount)); // Write Vout minutes
-      analogWrite(3, hrs_factor * (hours + hrsCount));
-    }
-  }
+  buttonLoop();
+  displayTime();
+  delay(100);
+}
