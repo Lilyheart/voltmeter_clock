dbda50b4b2372be66f8f9de04e2ee000ecca37f2
Lily Romano <Lilyheart@users.noreply.github.com>
2021-08-08 13:10:50 -0400

    Add calibration code

---
+++ calibration0/calibration0.ino
@@ -0,0 +1,15 @@
+const int secPin = 6;
+const int minPin = 5;
+const int hourPin = 3;
+
+void setup() {
+  // put your setup code here, to run once:
+  analogWrite(secPin, 0);
+  analogWrite(minPin, 0);
+  analogWrite(hourPin, 0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ calibration255/calibration255.ino
@@ -0,0 +1,15 @@
+const int secPin = 6;
+const int minPin = 5;
+const int hourPin = 3;
+
+void setup() {
+  // put your setup code here, to run once:
+  analogWrite(secPin, 255);
+  analogWrite(minPin, 255);
+  analogWrite(hourPin, 255);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
